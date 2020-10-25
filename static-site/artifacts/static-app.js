c109e933a02780431cf5a06976ad149869ce25a4
Valera Rozuvan <valera.rozuvan@gmail.com>
2020-10-25 23:16:07 +0200

    Update site.

--- static-site/artifacts/static-app.js
+++ static-site/artifacts/static-app.js
@@ -940,7 +940,7 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
 
 /* harmony default export */ __webpack_exports__["default"] = (function () {
-  return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", null, "React Static is a progressive static site generator for React."));
+  return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", null, "React Static is a progressive static site generator for React. Hello, world!"));
 });
 
 /***/ }),
--- static-site/src/pages/about.js
+++ static-site/src/pages/about.js
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default () => (
   <div>
-    <p>React Static is a progressive static site generator for React.</p>
+    <p>React Static is a progressive static site generator for React. Hello, world!</p>
   </div>
 )
